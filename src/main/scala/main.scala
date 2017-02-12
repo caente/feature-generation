3e287775c3b9da13096f6e79b78bb46be7bab717
Miguel Iglesias <caente@gmail.com>
2017-02-12 11:13:48 -0500

    labeless and contexless

--- src/main/scala/main.scala
+++ src/main/scala/main.scala
@@ -26,6 +26,8 @@ object Main {
       ) =
         fg.features(x, gen.to(context))
     }
+    case object Labeless
+    case class NoContext()
   }
 
   // features for String, each feature generator can process several contexts
@@ -50,10 +52,42 @@ object Main {
       (s, i) =>
         Map("sum" -> (s.size + i + x.size))
 
+    private def featureGenerator4(x: String): () => Map[String, Int] =
+      () =>
+        Map("size" -> (x.size))
+
     def features(x: String, context: Context): Map[String, Int] = {
       applyContext(context)(featureGenerator1(x)) ++
         applyContext(context)(featureGenerator2(x)) ++
-        applyContext(context)(featureGenerator3(x))
+        applyContext(context)(featureGenerator3(x)) ++
+        applyContext(context)(featureGenerator4(x))
+    }
+  }
+
+  implicit def LabelessFeatures[L <: HList, F](
+    implicit
+    si: Find[L, Int],
+    ss: Find[L, String],
+    sd: Find[L, Double]
+  ) = new FeatureGenerator[FeatureGenerator.Labeless.type] {
+    type Context = L
+
+    private def featureGenerator1: (Int, Double) => Map[String, Int] =
+      (i, d) =>
+        Map("ave" -> (d.toInt + i))
+
+    private def featureGenerator2: (Int) => Map[String, Int] =
+      i =>
+        Map("dev" -> i)
+
+    private def featureGenerator3: (String, Int) => Map[String, Int] =
+      (s, i) =>
+        Map("sum" -> (s.size + i))
+
+    def features(x: FeatureGenerator.Labeless.type, context: Context): Map[String, Int] = {
+      applyContext(context)(featureGenerator1) ++
+        applyContext(context)(featureGenerator2) ++
+        applyContext(context)(featureGenerator3)
     }
   }
 
@@ -61,18 +95,36 @@ object Main {
     import FeatureGenerator.Ops
     // it works with tuples and case classes
     assert(
-      "hi".features(1, 2d) == Map("ave" -> 3, "dev" -> 1)
+      "hi".features(FeatureGenerator.NoContext()) === Map("size" -> 2)
     )
     assert(
-      "hi".features("a", 1) == Map("sum" -> 4, "dev" -> 1)
+      "hi".features(1, 2d) == Map("ave" -> 3, "dev" -> 1, "size" -> 2)
+    )
+    assert(
+      "hi".features("a", 1) == Map("sum" -> 4, "dev" -> 1, "size" -> 2)
     )
     case class OneInt(i: Int)
     assert(
-      "hi".features(OneInt(1)) == Map("dev" -> 1)
+      "hi".features(OneInt(1)) == Map("dev" -> 1, "size" -> 2)
     )
     // the order of the arguments doesn't matter
     assert(
-      "hi".features(2d, 1, "a") == Map("sum" -> 4, "dev" -> 1, "ave" -> 3)
+      "hi".features(2d, 1, "a") == Map("sum" -> 4, "dev" -> 1, "ave" -> 3, "size" -> 2)
+    )
+
+    // feature generators with no X
+    assert(
+      FeatureGenerator.Labeless.features(1, 2d) == Map("ave" -> 3, "dev" -> 1)
+    )
+    assert(
+      FeatureGenerator.Labeless.features("a", 1) == Map("sum" -> 2, "dev" -> 1)
+    )
+    assert(
+      FeatureGenerator.Labeless.features(OneInt(1)) == Map("dev" -> 1)
+    )
+    // the order of the arguments doesn't matter
+    assert(
+      FeatureGenerator.Labeless.features(2d, 1, "a") == Map("sum" -> 2, "dev" -> 1, "ave" -> 3)
     )
 
   }
