18670e938975040acae508d9c73d9416380cc39d
Miguel Iglesias <caente@gmail.com>
2017-02-16 02:40:40 -0500

    applyAll

--- src/main/scala/main.scala
+++ src/main/scala/main.scala
@@ -30,46 +30,26 @@ object Main {
         featureGenerator3_1 ::
         featureGenerator4 :: HNil
 
-    def features[Context <: Product, HContext <: HList](context: Context)(
-      implicit
-      gen: Generic.Aux[Context, HContext],
-      distinct: IsDistinctConstraint[HContext],
-      si: Find[HContext, Int],
-      sc: Find[HContext, Char],
-      ss: Find[HContext, String],
-      sd: Find[HContext, Double]
-    ) =
-      applyAll(gen.to(context))(generators)
-
-    def features[X](x: X)(
-      implicit
-      si: Find[X :: HNil, Int],
-      sc: Find[X :: HNil, Char],
-      ss: Find[X :: HNil, String],
-      sd: Find[X :: HNil, Double]
-    ) =
-      applyAll(x :: HNil)(generators)
-
   }
 
   def main(args: Array[String]): Unit = {
     // it works with tuples and case classes
     val hi = "hi"
     assert(
-      FeatureGenerators.features(hi) == Seq("string_size" -> 2)
+      applyAll(hi)(FeatureGenerators.generators) == Seq("string_size" -> 2)
     )
     assert(
-      FeatureGenerators.features(hi, 1) == Seq("feature2" -> 1, "string_size" -> 2)
+      applyAll(hi, 1)(FeatureGenerators.generators) == Seq("feature2" -> 1, "string_size" -> 2)
     )
     assert(
-      FeatureGenerators.features(hi, 1, 2d) == Seq("feature1" -> 3, "feature2" -> 1, "string_size" -> 2)
+      applyAll(hi, 1, 2d)(FeatureGenerators.generators) == Seq("feature1" -> 3, "feature2" -> 1, "string_size" -> 2)
     )
     assert(
-      FeatureGenerators.features(hi, 'a', 1) == Seq("feature2" -> 1, "feature3" -> 100, "feature3_1" -> 101, "string_size" -> 2)
+      applyAll(hi, 'a', 1)(FeatureGenerators.generators) == Seq("feature2" -> 1, "feature3" -> 100, "feature3_1" -> 101, "string_size" -> 2)
     )
     // the order of the arguments doesn't matter
     assert(
-      FeatureGenerators.features(hi, 2d, 1, 'a') == Seq("feature1" -> 3, "feature2" -> 1, "feature3" -> 100, "feature3_1" -> 101, "string_size" -> 2)
+      applyAll(hi, 2d, 1, 'a')(FeatureGenerators.generators) == Seq("feature1" -> 3, "feature2" -> 1, "feature3" -> 100, "feature3_1" -> 101, "string_size" -> 2)
     )
   }
 }
